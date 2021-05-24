de02a5cc0e6ce173cf7777776fdfd33c3cc9383d
Jorge-Alda <jalda@unizar.es>
2021-05-24 18:41:36 +0200

    Reverted form factors to flavio v1.5

--- flavio/physics/bdecays/formfactors/b_p/cln.py
+++ flavio/physics/bdecays/formfactors/b_p/cln.py
@@ -1,4 +1,3 @@
-import flavio
 from flavio.physics.bdecays.formfactors import hqet
 from flavio.physics.bdecays.formfactors import common
 from math import sqrt
@@ -20,18 +19,17 @@ def h_to_f(mB, mP, h, q2):
     return ff
 
 
-def ff(process, q2, par, scale, order_z=3, order_z_slp=2, order_z_sslp=1):
+def ff(process, q2, par, scale):
     r"""Central value of $B\to P$ form factors in the lattice convention
     CLN parametrization.
 
     See arXiv:hep-ph/9712417 and arXiv:1703.05330.
     """
-    flavio.citations.register("Caprini:1997mu")
     pd = process_dict[process]
     mB = par['m_' + pd['B']]
     mP = par['m_' + pd['P']]
     w = max((mB**2 + mP**2 - q2) / (2 * mB * mP), 1)
-    phqet = hqet.get_hqet_parameters(par)
+    phqet = hqet.get_hqet_parameters(par, scale)
     ash = phqet['ash']
     epsc = phqet['epsc']
     epsb = phqet['epsb']
@@ -42,21 +40,18 @@ def ff(process, q2, par, scale, order_z=3, order_z_slp=2, order_z_sslp=1):
     CT1 = hqet.CT1(w, zc)
     CT2 = hqet.CT2(w, zc)
     CT3 = hqet.CT3(w, zc)
-    z = common.z(mB, mP, q2, t0='tm')
+    L = hqet.L(par, w)
+    # leading, universal Isgur-Wise function
     rho2 = par['CLN rho2_xi']
     c = par['CLN c_xi']
-    xi3 = par['CLN xi3']
-    xi = hqet.xi(z, rho2, c, xi3, order_z=order_z)
-    L = hqet.Lz(par, w, z, order_z=order_z_slp)
-    ell = hqet.ell(par, z, order_z=order_z_sslp)
+    z = common.z(mB, mP, q2, t0='tm')
+    xi = 1 - 8 * rho2 * z + (64 * c - 16 * rho2) * z**2
     h = {}
     h['h+'] = xi * (1 + ash * (CV1 + (w + 1) / 2 * (CV2 + CV3))
                     + (epsc + epsb) * L[1]
-                    + epsc**2 * ell[1])
+                    + epsc**2 * par['B->D CLN deltac_h+'])
     h['h-'] = xi * (ash * (w + 1) / 2 * (CV2 - CV3)
-                    + (epsc - epsb) * L[4]
-                    + epsc**2 * ell[4])
+                    + (epsc - epsb) * L[4])
     h['hT'] = xi * (1 + ash * (CT1 - CT2 + CT3)
-                    + (epsc + epsb) * (L[1] - L[4])
-                    + epsc**2 * (ell[1] - ell[4]))
+                    + (epsc + epsb) * (L[1] - L[4]))
     return h_to_f(mB, mP, h, q2)
